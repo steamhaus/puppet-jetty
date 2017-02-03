4ba83f68b9b06d877b14518195cb5e04d7aea394
Javier Juarez <javier.juarez@gmail.com>
2017-02-03 14:15:25 +0100

    Better documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,43 @@
 ##
-# = class: jetty - This class helps to install a Jetty Web Server
+# = Class: jetty
+#
+# == Parameters:
+#
+# $root:: The root path of the installation, default is /opt
+# $base:: The JETTY_BASE web deployment
+# $version:: The version of the jetty server
+# $service_ensure:: Offers the opportinity to manage the service
+# $manage_user:: Offers the opportunity to manage the group and user
+# $user:: The name of the user
+# $group:: The name of the group
+# $mirror:: The mirror base URL
+# $archive_type:: ('.zip', '.tar.gz')
+# $checksum_type:: ('sha1', 'md5')
+# $jetty_arguments:: A list of additional arguments for the jetty process
+# $java:: The java command to launch jetty server
+# $java_options:: Additional options to the JVM
+# $configuration:: A hash to configure sereval modules of the jetty server
+# logconfig:: A hash to configure log4j logging system
+#
+# == Requires:
+#
+# Java
+#
+# == Sample Usage:
+#
+# class { 'jetty':
+#   version => '9.2.21.v20170120',
+# }
+#
+#
+# == Authors
+#
+# Javier Juarez
+#
+# == Copyright
+#
+# This software is distributed under Apache 2.0 license
+#
 class jetty(
   Stdlib::Absolutepath      $root,
   Stdlib::Absolutepath      $base,
